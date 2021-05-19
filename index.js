2fbbed3bda8b635418461acb0644046fb680a51e
tranghasgit <56140172+tranghasgit@users.noreply.github.com>
2021-05-19 23:07:25 +0200

    vimeo

--- index.js
+++ index.js
@@ -1,94 +1,32 @@
-// $(window).scroll(function() {
-//   sessionStorage.scrollTop = $(this).scrollTop();
-// });
-
 $(document).ready(function(){
 
-  // keep same scroll position after reload
-  // if (sessionStorage.scrollTop != "undefined") {
-  //   $(window).scrollTop(sessionStorage.scrollTop);
-  // }
-  var outrHeight = 0;
-  $("img, video").each(function(){
-    outrHeight += $(this).outerHeight();
-  });
-  console.log(outrHeight);
+  // IMPROVE IMAGE LOADING and loading screen
+  // Images loaded is zero because we're going to process a new set of images.
+    var imagesLoaded = 0
+    // Total images is still the total number of <img> elements on the page.
+    var totalImages = $("img").length;
 
-  // parallax effect
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    smartphone: { smooth: true },
-    tablet: { smooth: true },
-  });
+    // Step through each image in the DOM, clone it, attach an onload event
+    // listener, then set its source to the source of the original image. When
+    // that new image has loaded, fire the imageLoaded() callback.
+    $("img").each(function (idx, img) {
+      $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"))
+    })
 
+    // Do exactly as we had before -- increment the loaded count and if all are
+    // loaded, call the allImagesLoaded() function.
+    function imageLoaded() {
+      imagesLoaded++
+      if (imagesLoaded == totalImages) {
+        allImagesLoaded()
+      }
+    }
 
-  // gallery lightbox function
-  $(".scroll-item").click(function(){
-    $("#gallery-popup").css({
-      "overflow-x" : "auto",
-    });
-    $('#scroll-wrapper').css({
-      "overflow-y" : "hidden"
-    });
+    function allImagesLoaded() {
+      $("#gallery-popup").hide();
+      $("#loading-screen").delay("200").fadeOut("slow");
+    }
 
-    // var randomAud = Math.floor(Math.random() * 3)
-    // var audio = $(".audio");
-    // audio.get(randomAud).play();
-    // console.log(randomAud);
-
-
-    // find index of clicked image
-    var i = $(this).data('index');
-
-    // open gallery lightbox
-    $("#gallery-popup").fadeIn();
-    $("#gallery-popup > .close-button").fadeIn();
-    $(".gallery-wrapper").scrollLeft(0); // reset scroll position
-    $('[data-target]:not(:last-child)').css({
-      "margin-right" : '.5em',
-      "margin-left" : '0em',
-    });
-    $('[data-target]').css({
-      "opacity" : .25,
-    });
-
-
-    // scroll target image to correct position determined by clicked image
-    var target = $("#gallery-popup").find("[data-target='" + i + "']");
-    var outerContW = $(".gallery-wrapper").outerWidth();
-    var targetW = target.outerWidth();
-    var offCenter = (outerContW - targetW) / 2;
-    var offset = target.offset().left - offCenter;
-    $(".gallery-wrapper").scrollLeft(offset);
-
-    target.css({
-      // "margin-right" : targetW,
-      "opacity" : 1,
-    });
-
-
-    $('[data-target]').each(function(){
-      $(this).click(function(){
-        $(this).animate({
-          // "margin-right" : $(this).outerWidth(),
-          "opacity" : 1,
-        });
-      });
-    });
-  });
-
-  // close lightbox
-  $("#gallery-popup > .close-button").click(function(){
-    $("#gallery-popup").fadeOut();
-    $("#gallery-popup").css({
-      "overflow-x" : "hidden",
-    });
-    $(this).fadeOut();
-    $('#scroll-wrapper').css({
-      "overflow-y" : "auto",
-    });
-  });
 
   // add beads to page
   const beadsSet1 = 14;
@@ -103,17 +41,64 @@ $(document).ready(function(){
     var randPosLH = Math.floor(Math.random() * ($("#scroll-wrapper").height() - beadH));
     var xPos = 50 + 80 * Math.sin((i / 80) * 5 * Math.PI);
 
-    $("#scroll-wrapper").append(newDiv);
+    $("body").append(newDiv);
     $(".bead-" + i).css({
       "position":"absolute",
       "top": randPosLH,
-      // "top": i * beadH + beadsOffsetY,
       "right" : randPosLR,
       "width": beadW,
       "height": beadH,
-      // "background-color":"#000"
     });
   }
+
+
+  // parallax effect
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    smartphone: { smooth: true },
+    tablet: { smooth: true },
+  });
+
+
+  // image slider
+  var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    "responsive": {
+      "500": {
+        "items": 3,
+        "edgePadding": 50,
+        "gutter": 10,
+      },
+      // "780": {
+      //   "items": 5
+      // }
+    },
+    slideBy: 1,
+    mouseDrag: true,
+    arrowKeys: true,
+    controls: false,
+    nav: false,
+    center: true,
+    "edgePadding": 0,
+    "lazyload": true,
+    autoplay: false
+  });
+
+    $(".scroll-item").click(function(){
+      var i = $(this).data('index');
+      slider.goTo(i);
+      $("[data-target="+i+"]").css({"opacity" : 1,});
+      $("#gallery-popup").fadeIn("slow");
+      $("[data-target]").click(function(){
+        $(this).animate({"opacity" : 1,});
+      });
+    });
+    $(".close-button").click(function(){
+      $("#gallery-popup").fadeOut("slow");
+    });
+
 
   $(".dropdown-info-content, .dropdown-about-content, .dropdown-contact-content").hide();
   $(".dropdown-info p:first-child").click(function(ev){
