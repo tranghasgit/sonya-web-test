780f973e4fb4701589112d2d3ece2a50f8ae84ca
tranghasgit <56140172+tranghasgit@users.noreply.github.com>
2021-05-20 00:40:22 +0200

    again

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 $(document).ready(function(){
 
-  // IMPROVE IMAGE LOADING and loading screen
-  // Images loaded is zero because we're going to process a new set of images.
-    var imagesLoaded = 0
-    // Total images is still the total number of <img> elements on the page.
-    var totalImages = $("img").length;
-
-    // Step through each image in the DOM, clone it, attach an onload event
-    // listener, then set its source to the source of the original image. When
-    // that new image has loaded, fire the imageLoaded() callback.
-    $("img").each(function (idx, img) {
-      $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"))
-    })
-
-    // Do exactly as we had before -- increment the loaded count and if all are
-    // loaded, call the allImagesLoaded() function.
-    function imageLoaded() {
-      imagesLoaded++
-      if (imagesLoaded == totalImages) {
-        allImagesLoaded()
-      } else {
-        notAllImagesLoaded()
-      }
-    }
-
-    function allImagesLoaded() {
-      $("#gallery-popup").hide();
-      $("#loading-screen").delay("200").fadeOut("slow");
-    }
-    function notAllImagesLoaded() {
-      $("#gallery-popup").hide();
-      $("#loading-screen").click(function(){
-        $(this).fadeOut(function(){
-          $(this).remove();
-        });
-      });
-    }
-
-  $("#gallery-popup").hide();
-
+  // // IMPROVE IMAGE LOADING and loading screen
+  // // Images loaded is zero because we're going to process a new set of images.
+  //   var imagesLoaded = 0
+  //   // Total images is still the total number of <img> elements on the page.
+  //   var totalImages = $("img").length;
+  //
+  //   // Step through each image in the DOM, clone it, attach an onload event
+  //   // listener, then set its source to the source of the original image. When
+  //   // that new image has loaded, fire the imageLoaded() callback.
+  //   $("img").each(function (idx, img) {
+  //     $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"))
+  //   })
+  //
+  //   // Do exactly as we had before -- increment the loaded count and if all are
+  //   // loaded, call the allImagesLoaded() function.
+  //   function imageLoaded() {
+  //     imagesLoaded++
+  //     if (imagesLoaded == totalImages) {
+  //       allImagesLoaded()
+  //     } else {
+  //       notAllImagesLoaded()
+  //     }
+  //   }
+  //
+  //   function allImagesLoaded() {
+  //     $("#gallery-popup").hide();
+  //     $("#loading-screen").delay("200").fadeOut("slow");
+  //   }
+  //   function notAllImagesLoaded() {
+  //     $("#gallery-popup").hide();
+  //     $("#loading-screen").delay("200").fadeOut("slow");
+  //   }
+  //
+  // $("#gallery-popup").hide();
+  // $("#loading-screen").delay("200").fadeOut("slow");
+  //
 
 
   // add beads to page
