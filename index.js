eed7b49c9531ffd48a67b19153ab11889e61882e
tranghasgit <56140172+tranghasgit@users.noreply.github.com>
2021-05-20 11:34:09 +0200

    functions

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 $(document).ready(function(){
 
-  // // IMPROVE IMAGE LOADING and loading screen
-  // // Images loaded is zero because we're going to process a new set of images.
-  //   var imagesLoaded = 0
-  //   // Total images is still the total number of <img> elements on the page.
-  //   var totalImages = $("img").length;
-  //
-  //   // Step through each image in the DOM, clone it, attach an onload event
-  //   // listener, then set its source to the source of the original image. When
-  //   // that new image has loaded, fire the imageLoaded() callback.
-  //   $("img").each(function (idx, img) {
-  //     $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"))
-  //   })
-  //
-  //   // Do exactly as we had before -- increment the loaded count and if all are
-  //   // loaded, call the allImagesLoaded() function.
-  //   function imageLoaded() {
-  //     imagesLoaded++
-  //     if (imagesLoaded == totalImages) {
-  //       allImagesLoaded()
-  //     } else {
-  //       notAllImagesLoaded()
-  //     }
-  //   }
-  //
-  //   function allImagesLoaded() {
-  //     $("#gallery-popup").hide();
-  //     $("#loading-screen").delay("200").fadeOut("slow");
-  //   }
+  // IMPROVE IMAGE LOADING and loading screen
+  // Images loaded is zero because we're going to process a new set of images.
+    var imagesLoaded = 0
+    // Total images is still the total number of <img> elements on the page.
+    var totalImages = $("img").length;
+
+    // Step through each image in the DOM, clone it, attach an onload event
+    // listener, then set its source to the source of the original image. When
+    // that new image has loaded, fire the imageLoaded() callback.
+    $("img").each(function (idx, img) {
+      $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"))
+    })
+
+    // Do exactly as we had before -- increment the loaded count and if all are
+    // loaded, call the allImagesLoaded() function.
+    function imageLoaded() {
+      imagesLoaded++
+      if (imagesLoaded == totalImages) {
+        allImagesLoaded()
+      } else {
+        // notAllImagesLoaded()
+      }
+    }
+
+    function allImagesLoaded() {
+      $("#gallery-popup").hide();
+      $("#loading-screen").delay("200").fadeOut("slow");
+    }
   //   function notAllImagesLoaded() {
   //     $("#gallery-popup").hide();
   //     $("#loading-screen").delay("200").fadeOut("slow");
@@ -71,44 +71,44 @@ $(document).ready(function(){
   });
 
 
-  // image slider
-  // var slider = tns({
-  //   container: '.my-slider',
-  //   items: 1,
-  //   "responsive": {
-  //     "600": {
-  //       "items": 3,
-  //       "edgePadding": 50,
-  //       "gutter": 10,
-  //     },
-  //     // "780": {
-  //     //   "items": 5
-  //     // }
-  //   },
-  //   slideBy: 1,
-  //   mouseDrag: false,
-  //   arrowKeys: true,
-  //   controls: false,
-  //   nav: false,
-  //   center: true,
-  //   "edgePadding": 0,
-  //   "lazyload": true,
-  //   autoplay: false
-  // });
-  //
-  //   $(".scroll-item").click(function(){
-  //     var i = $(this).data('index');
-  //     slider.goTo(i);
-  //     $("[data-target="+i+"]").css({"opacity" : 1,});
-  //     $("#gallery-popup").fadeIn("slow");
-  //     $(".close-button").fadeIn("slow");
-  //     $("[data-target]").click(function(){
-  //       $(this).animate({"opacity" : 1,});
-  //     });
-  //   });
-  //   $(".close-button").click(function(){
-  //     $("#gallery-popup").fadeOut("slow");
-  //   });
+  image slider
+  var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    "responsive": {
+      "600": {
+        "items": 3,
+        "edgePadding": 50,
+        "gutter": 10,
+      },
+      // "780": {
+      //   "items": 5
+      // }
+    },
+    slideBy: 1,
+    mouseDrag: false,
+    arrowKeys: true,
+    controls: false,
+    nav: false,
+    center: true,
+    "edgePadding": 0,
+    "lazyload": true,
+    autoplay: false
+  });
+
+    $(".scroll-item").click(function(){
+      var i = $(this).data('index');
+      slider.goTo(i);
+      $("[data-target="+i+"]").css({"opacity" : 1,});
+      $("#gallery-popup").fadeIn("slow");
+      $(".close-button").fadeIn("slow");
+      $("[data-target]").click(function(){
+        $(this).animate({"opacity" : 1,});
+      });
+    });
+    $(".close-button").click(function(){
+      $("#gallery-popup").fadeOut("slow");
+    });
 
 
   $(".dropdown-info-content, .dropdown-about-content, .dropdown-contact-content").hide();
